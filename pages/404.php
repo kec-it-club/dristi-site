c069dc07c3e1752b2263b3da6d48f7b82168f0f2
aryaminus <sunim.54@gmail.com>
2019-06-21 23:49:16 +0545

    Add Building over jpg

--- pages/404.php
+++ pages/404.php
@@ -73,7 +73,7 @@ define("PAGE", Pages::HOME)
     <? include 'include/nav.php'; ?>
 
 
-    <div class="hero-wrap js-fullheight" style="background-image: url('/images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap js-fullheight" style="background-image: url('/images/building.jpg');" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
