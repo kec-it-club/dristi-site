905fc8e31251a0e2a955683ee41e7ca1876afdd3
suyogdahal <suyogdahal46@gmail.com>
2019-06-21 20:28:01 +0545

    Added rest of the events

---
+++ pages/events/earthquake-resistant.php
@@ -0,0 +1,201 @@
+<? defined("CHECK") OR ( header( 'Location: /' ) && exit ); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Dristi 2.0: Earthquake resistant design competition</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- {/* Item prop */} -->
+  <meta itemprop="name" content="Dristi2.0" />
+  <meta itemprop="description" content="The Vision beyond imagination" />
+  <meta itemprop="image" content="https://dristi.keckist.edu.np/social.jpg" />
+
+  <!-- {/* Twitter */} -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Dristi2.0" />
+  <meta name="twitter:description" content="The Vision beyond imagination" />
+  <meta name="twitter:site" content="@dristi" />
+  <meta name="twitter:creator" content="@dristi" />
+  <meta name="twitter:image:src" content="https://dristi.keckist.edu.np/social.jpg" />
+  <!-- {/* Twitter */} -->
+
+  <!-- {/* Facebook */} -->
+  <meta property="og:title" content="Dristi2.0" />
+  <meta property="og:description" content="The Vision beyond imagination" />
+  <meta property="og:image" content="https://dristi.keckist.edu.np/social.jpg" />
+  <meta property="og:url" content="https://dristi.keckist.edu.np" />
+  <meta property="og:site_name" content="Dristi2.0" />
+  <meta property="og:type" content="website" />
+  <!-- {/* Facebook */} -->
+
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
+  <link rel="stylesheet" href="../css/animate.css">
+
+  <link rel="stylesheet" href="../css/owl.carousel.min.css">
+  <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="../css/magnific-popup.css">
+
+  <link rel="stylesheet" href="../css/aos.css">
+
+  <link rel="stylesheet" href="../css/ionicons.min.css">
+
+  <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="../css/jquery.timepicker.css">
+
+
+  <link rel="stylesheet" href="../css/flaticon.css">
+  <link rel="stylesheet" href="../css/icomoon.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+<? include __DIR__.'/../include/nav.php'; ?>
+  <!-- END nav -->
+
+  <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../social.jpg');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+        <div class="col-md-9 ftco-animate pb-5">
+          <!-- todo -->
+          <h1 class="mb-3 bread">Earthquake resistant design competition</h1>
+          <p class="breadcrumbs">
+            <span class="mr-2"> <a href="/">Home <i class="ion-ios-arrow-forward"></i></a></span>
+            <span class="mr-2"> <a href="/events">Events <i class="ion-ios-arrow-forward"></i></a></span>
+            <span>Earthquake resistant design competition<i class="ion-ios-arrow-forward"></i></span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section ftco-degree-bg">
+    <div class="container">
+      <div class="row">
+        <!-- todo -->
+        <div class="col-md-10 order-md-last ftco-animate">
+          <!-- <h2 class="mb-3">Introduction</h2>
+
+          <p>
+            <img src="../images/hackathon.jpg" alt="" class="img-fluid">
+          </p>
+
+          <p style="color:#373c41;">Inter-college/Intra-college Quiz Competition is an event where various students from different colleges will participate
+           in a competition. Various objective competition would be asked by quiz masters according to rules and regulations set by the organizing committee.
+           This event helps students to broaden their knowledge which would be very useful in various fields.
+          </p> -->
+          <!-- <h2 class="mb-3 mt-5">Event Details:</h2>
+          <ul>
+            <li style="color:#373c41;"> <b>Venue</b>: Kathmandu Engineering College</li>
+            <li style="color:#373c41;"> <b>Participation</b>: As a team of three members (Registration Fee: Rs. 1000/team)</li>
+            <li style="color:#373c41;"> <b>Type of Event</b>: Competition</li>
+          </ul> -->
+
+
+          <h2 class="mb-3 mt-5">General Specification:</h2>
+          <ul>
+            <li style="color:#373c41;">The structure must be able to stand on its own.</li>
+            <li style="color:#373c41;">The loads will be added to every floor by the event organizers and the structure must be able to withstand the loads.</li>
+            <li style="color:#373c41;">The structure must be able to withstand the vibration from beneath</li>
+            <li style="color:#373c41;">The floor area must be less than the base area.</li>
+            <li style="color:#373c41;">The superstructure must be attached to the base.</li>
+            <li style="color:#373c41;">
+            The building must have following dimensions:
+                <ul>
+                    <li style="color:#373c41;"><b>Building height</b>: 1’6” – 2’</li>
+                    <li style="color:#373c41;"><b>Base area</b>: 150- 200 sq. in</li>
+                    <li style="color:#373c41;"><b>Number of floors</b>: not less than 5</li>
+                </ul>
+            </li>
+            <li style="color:#373c41;">The teams must submit their models 1- 1.5 hours before the competition for inspection.</li>
+            <li style="color:#373c41;">Teams will be given 15 minutes to modify the model which do not qualify the inspection.</li>
+            <li style="color:#373c41;">Qualified model will be placed on the table by the team members who may make final adjustments in its position.</li>
+            <li style="color:#373c41;">Once the model is in position the loading will be placed on every floor and vibration will be applied from beneath.</li>
+            <li style="color:#373c41;">Judging will be based on the last standing structure.</li>
+            <li style="color:#373c41;">The building should also be aesthetically appealing. Some marks are allocated for the same.</li>
+            <li style="color:#373c41;">
+            Materials that can be used are:
+                <ul>
+                    <li style="color:#373c41;">Balsa wood strips/ MDF timber strips.</li>
+                    <li style="color:#373c41;">Papers</li>
+                    <li style="color:#373c41;">Base</li>
+                    <li style="color:#373c41;">Adhesives like fevicol, movicol and hot glue can be used.</li>
+                </ul>
+            </li>
+            <li style="color:#373c41;">Weight of the building must not exceed 1500gm.
+</li>
+
+
+          </ul>
+
+          <h2 class="mb-3 mt-5">Registration Information:</h2>
+          <ul>
+            <li style="color:#373c41;">A team must register their names for ‘EARTHQUAKE RESISTANT DESIGN COMPETITION’ with entry fee of NRs. 3000 by filling up the registration form given by the organizer.</li>
+            <li style="color:#373c41;">A team must have at least 5 members</li>
+          </ul>
+
+
+          <h2 class="mb-3 mt-5">Awards :</h2>
+          <ul>
+
+
+            <li style="color:#373c41;">Winner will be awarded with Rs. 15000 cash prize and runner up will be awarded with Rs. 10000 respectively.</li>
+            <li style="color:#373c41;"> All the winners will also be awarded with medals and certificates.</li>
+            <li style="color:#373c41;"> All the participants will be awarded with the certificate of participation.</li>
+
+          </ul>
+          <!-- <h2 class="mb-3 mt-5">Prize:</h2>
+          <ul>
+            <li style="color:#373c41;"> <b>Winner</b>: Rs. 5,000 </li>
+            <li style="color:#373c41;"> <b>Second place</b>: Rs. 3,000
+            </li>
+            <li style="color:#373c41;"> <b>Third place:</b> Rs. 2,000 </li>
+          </ul> -->
+         
+          <!-- <h2 class="mb-3 mt-5">Contact:</h2>
+          <ul>
+            <li style="color:#373c41;">Binod Acharya: 9860920775 / 9812815991</li>
+          </ul> -->
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <? include __DIR__.'/../include/footer.php'; ?>
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#F96D00" /></svg></div>
+
+
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+  <script src="../js/jquery.waypoints.min.js"></script>
+  <script src="../js/jquery.stellar.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/aos.js"></script>
+  <script src="../js/jquery.animateNumber.min.js"></script>
+  <script src="../js/bootstrap-datepicker.js"></script>
+  <script src="../js/scrollax.min.js"></script>
+  <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../js/google-map.js"></script>
+  <script src="../js/main.js"></script>
+
+</body>
+
+</html>
---
+++ pages/events/paper-bridge.php
@@ -0,0 +1,187 @@
+<? defined("CHECK") OR ( header( 'Location: /' ) && exit ); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Dristi 2.0: Paper Bridge</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- {/* Item prop */} -->
+  <meta itemprop="name" content="Dristi2.0" />
+  <meta itemprop="description" content="The Vision beyond imagination" />
+  <meta itemprop="image" content="https://dristi.keckist.edu.np/social.jpg" />
+
+  <!-- {/* Twitter */} -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Dristi2.0" />
+  <meta name="twitter:description" content="The Vision beyond imagination" />
+  <meta name="twitter:site" content="@dristi" />
+  <meta name="twitter:creator" content="@dristi" />
+  <meta name="twitter:image:src" content="https://dristi.keckist.edu.np/social.jpg" />
+  <!-- {/* Twitter */} -->
+
+  <!-- {/* Facebook */} -->
+  <meta property="og:title" content="Dristi2.0" />
+  <meta property="og:description" content="The Vision beyond imagination" />
+  <meta property="og:image" content="https://dristi.keckist.edu.np/social.jpg" />
+  <meta property="og:url" content="https://dristi.keckist.edu.np" />
+  <meta property="og:site_name" content="Dristi2.0" />
+  <meta property="og:type" content="website" />
+  <!-- {/* Facebook */} -->
+
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
+  <link rel="stylesheet" href="../css/animate.css">
+
+  <link rel="stylesheet" href="../css/owl.carousel.min.css">
+  <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="../css/magnific-popup.css">
+
+  <link rel="stylesheet" href="../css/aos.css">
+
+  <link rel="stylesheet" href="../css/ionicons.min.css">
+
+  <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="../css/jquery.timepicker.css">
+
+
+  <link rel="stylesheet" href="../css/flaticon.css">
+  <link rel="stylesheet" href="../css/icomoon.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+<? include __DIR__.'/../include/nav.php'; ?>
+  <!-- END nav -->
+
+  <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../social.jpg');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+        <div class="col-md-9 ftco-animate pb-5">
+          <!-- todo -->
+          <h1 class="mb-3 bread">Paper Bridge</h1>
+          <p class="breadcrumbs">
+            <span class="mr-2"> <a href="/">Home <i class="ion-ios-arrow-forward"></i></a></span>
+            <span class="mr-2"> <a href="/events">Events <i class="ion-ios-arrow-forward"></i></a></span>
+            <span>Paper Bridge<i class="ion-ios-arrow-forward"></i></span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section ftco-degree-bg">
+    <div class="container">
+      <div class="row">
+        <!-- todo -->
+        <div class="col-md-10 order-md-last ftco-animate">
+          <!-- <h2 class="mb-3">Introduction</h2>
+
+          <p>
+            <img src="../images/hackathon.jpg" alt="" class="img-fluid">
+          </p>
+
+          <p style="color:#373c41;">Inter-college/Intra-college Quiz Competition is an event where various students from different colleges will participate
+           in a competition. Various objective competition would be asked by quiz masters according to rules and regulations set by the organizing committee.
+           This event helps students to broaden their knowledge which would be very useful in various fields.
+          </p> -->
+          <!-- <h2 class="mb-3 mt-5">Event Details:</h2>
+          <ul>
+            <li style="color:#373c41;"> <b>Venue</b>: Kathmandu Engineering College</li>
+            <li style="color:#373c41;"> <b>Participation</b>: As a team of three members (Registration Fee: Rs. 1000/team)</li>
+            <li style="color:#373c41;"> <b>Type of Event</b>: Competition</li>
+          </ul> -->
+
+
+          <h2 class="mb-3 mt-5">General Specification:</h2>
+          <ul>
+            <li style="color:#373c41;">The dimension of the bridge should be (50*5) cm with a tolerance of  ±1cm in both length and width.</li>
+            <li style="color:#373c41;">Support span should be 5 cm on each side with a tolerance of ±1 cm.</li>
+            <li style="color:#373c41;">Deflection should be within 4 cm from the base of the bridge.</li>
+            <li style="color:#373c41;">Central deflection will be considered.</li>
+            <li style="color:#373c41;">Concentrated load will be applied centrally.</li>
+            <li style="color:#373c41;">
+            Participants are not allowed to use the materials other than specified by the host:
+                <ul>
+                    <li style="color:#373c41;">2 kg of paper, paper cutter shall be provided by the organizers to the participants.</li>
+                    <li style="color:#373c41;">Adhesives will also be given to the participants.</li>
+                </ul>
+            </li>
+            <li style="color:#373c41;">A team can have at most 3 members.</li>
+            <li style="color:#373c41;">Maximum allowable bridge weight is 800 gm.</li>
+            <li style="color:#373c41;">Bridges will be compared in ‘Strength to weight’ ratio. The weight of testing device is not included in the final weight.</li>
+            <li style="color:#373c41;">The bridge should also be aesthetically appealing. Some marks are allocated for the same.</li>
+            <li style="color:#373c41;">It is recommended that you weigh your Paper bridge before the testing of the bridge.</li>
+
+          </ul>
+
+          <h2 class="mb-3 mt-5">Registration Information:</h2>
+          <ul>
+            <li style="color:#373c41;">A participant must register their names for ‘PAPER BRIDGE’ with entry fee of NRs. 2000 by filling up the registration form given by the organizer. </li>
+          </ul>
+
+
+          <h2 class="mb-3 mt-5">Awards :</h2>
+          <ul>
+
+
+            <li style="color:#373c41;">Winner will be awarded with Rs. 6000 cash prize and runner up will be awarded with Rs. 4000 respectively.</li>
+            <li style="color:#373c41;"> All the winners will also be awarded with medals and certificates.</li>
+            <li style="color:#373c41;"> All the participants will be awarded with the certificate of participation.
+</li>
+
+          </ul>
+          <!-- <h2 class="mb-3 mt-5">Prize:</h2>
+          <ul>
+            <li style="color:#373c41;"> <b>Winner</b>: Rs. 5,000 </li>
+            <li style="color:#373c41;"> <b>Second place</b>: Rs. 3,000
+            </li>
+            <li style="color:#373c41;"> <b>Third place:</b> Rs. 2,000 </li>
+          </ul> -->
+         
+          <h2 class="mb-3 mt-5">Contact:</h2>
+          <ul>
+            <li style="color:#373c41;">Binod Acharya: 9860920775 / 9812815991</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <? include __DIR__.'/../include/footer.php'; ?>
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#F96D00" /></svg></div>
+
+
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+  <script src="../js/jquery.waypoints.min.js"></script>
+  <script src="../js/jquery.stellar.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/aos.js"></script>
+  <script src="../js/jquery.animateNumber.min.js"></script>
+  <script src="../js/bootstrap-datepicker.js"></script>
+  <script src="../js/scrollax.min.js"></script>
+  <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../js/google-map.js"></script>
+  <script src="../js/main.js"></script>
+
+</body>
+
+</html>
