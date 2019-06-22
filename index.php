dc3bdee5b06fdcee407229c3cda39b31ba833b8c
psuzn <spoudel347@gmail.com>
2019-06-22 17:31:49 +0545

    new router

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 define("CHECK", true);
 
-$request = $_SERVER["REQUEST_URI"];
+$request =$_SERVER["REQUEST_URI"];
 
-if (preg_match('/^\/events\/([a-zA-Z0-9_-]+)*$/', $request, $output_array)) {
-    $dir = __DIR__ . '/pages/events/' . $output_array[1]. ".php";
+if (preg_match('/^\/events\/([a-zA-Z0-9_-]+)([?]+.*)*$/', $request, $output_array)) {
+    $dir = __DIR__ . '/pages/events/' . $output_array[1] . ".php";
 
     // var_dump($dir);
     // var_dump(file_exists($dir));
@@ -15,21 +15,63 @@ if (preg_match('/^\/events\/([a-zA-Z0-9_-]+)*$/', $request, $output_array)) {
         exit;
     }
 }
-switch ($request) {
-    case '':
-    case '/':
-        require __DIR__ . '/pages/index.php';
-        break;
-    case '/about':
-        require __DIR__ . '/pages/about.php';
-        break;
-    case '/events':
-        require __DIR__ . '/pages/events.php';
-        break;
-    case '/contact-us':
-        require __DIR__ . '/pages/contact-us.php';
-        break;
-    default:
-        require __DIR__ . '/pages/404.php';
-        break;
+
+$routes = array(
+    "" => "/pages/index.php",
+    "/" => "/pages/index.php",
+    "/about" => "/pages/about.php",
+    "/events" => "/pages/events.php",
+    "/contact-us" => "/pages/contact-us.php"
+);
+
+foreach ($routes as $route => $path) {
+    $route = preg_quote($route,"/");
+    $regx = "/^". $route ."([?]+.*)*$/";
+    if (preg_match($regx, $request, $output_array)) {
+        require __DIR__ . $path;
+        exit;
+    }
 }
+
+require __DIR__ . '/pages/404.php';
+exit;
+
+
+
+/* old router doesnt supports get parameters */
+
+// define("CHECK", true);
+
+// $request =$_SERVER["REQUEST_URI"];
+
+// if (preg_match('/^\/events\/([a-zA-Z0-9_-]+)([?]+.*)*$/', $request, $output_array)) {
+//     $dir = __DIR__ . '/pages/events/' . $output_array[1] . ".php";
+
+//     // var_dump($dir);
+//     // var_dump(file_exists($dir));
+//     // die;
+
+//     if (file_exists($dir)) {
+//         require $dir;
+//         exit;
+//     }
+// }
+
+// switch ($request) {
+//     case '':
+//     case '/':
+//         require __DIR__ . '/pages/index.php';
+//         break;
+//     case '/about':
+//         require __DIR__ . '/pages/about.php';
+//         break;
+//     case '/events':
+//         require __DIR__ . '/pages/events.php';
+//         break;
+//     case '/contact-us':
+//         require __DIR__ . '/pages/contact-us.php';
+//         break;
+//     default:
+//         require __DIR__ . '/pages/404.php';
+//         break;
+// }
