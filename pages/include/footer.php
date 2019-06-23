123b29c0cbe3bfd517d8354e0754f2bc25a7e50b
psuzn <spoudel347@gmail.com>
2019-06-23 07:22:20 +0545

    google analytics

--- pages/include/footer.php
+++ pages/include/footer.php
@@ -60,3 +60,13 @@
         </div>
     </div>
 </footer>
+
+
+<!--  Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142516091-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-142516091-1');
+</script>
